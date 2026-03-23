15f514e3831320f2aaca1f531875f0fd44ff1d90
Iyán Díaz Pereda <uo300006@uniovi.es>
2026-03-23 19:52:04 +0100

    mejoras en navegabilidad

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route} from "react-router-dom";
 import AuthForm from "./components/AuthForm/LoginForm";
 import Game from "./pages/Game";
 import Lobby from "./pages/Lobby";
 
 function App() { 
-  
+    
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthForm />} />
-        <Route path="/lobby" element={<Lobby />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-    </BrowserRouter>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<AuthForm />} />
+          <Route path="/lobby" element={<Lobby />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </HashRouter>
   );
 }
 
--- webapp/src/components/AuthForm/LoginForm.tsx
+++ webapp/src/components/AuthForm/LoginForm.tsx
@@ -1,9 +1,8 @@
 import './AuthForm.css'
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthForm: React.FC = () => {
-  const userLogged = localStorage.getItem("username") ?? undefined;
   const [isLogin, setIsLogin] = useState(true);
   const navigate = useNavigate();
 
@@ -11,12 +10,6 @@ const AuthForm: React.FC = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-          if (userLogged != null) {
-              navigate('/lobby');
-          }
-      }, [username, navigate]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
